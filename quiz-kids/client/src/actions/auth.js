9665eb79347c1782453ce6f7b2fca5d1f6615683
fog9999 <fog9999>
2022-11-26 16:20:43 +0700

    bug 7

--- quiz-kids/client/src/actions/auth.js
+++ quiz-kids/client/src/actions/auth.js
@@ -1,5 +1,7 @@
 import * as api from "../api";
 import { AUTH } from "../constants/actionTypes";
+import { createSocket } from "./socket";
+import { io } from "socket.io-client"
 
 export const login = (formData, history) => async (dispatch) => {
   try {
@@ -9,6 +11,8 @@ export const login = (formData, history) => async (dispatch) => {
       history.push("/admin");
     }
     else history.push("/");
+    const socket = io("http://localhost:3001")
+    dispatch(createSocket(socket))
   } catch (error) {
     console.log(error);
   }
@@ -19,6 +23,8 @@ export const register = (formData, history) => async (dispatch) => {
     const { data } = await api.register(formData);
     dispatch({ type: AUTH, data });
     history.push("/");
+    const socket = io("http://localhost:3001")
+    dispatch(createSocket(socket))
   } catch (error) {
     console.log(error);
   }
